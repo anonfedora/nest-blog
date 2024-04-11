659a259bd22f2fe148832117521c398918d49580
anonfedora <musaeleazar090@gmail.com>
2024-04-11 23:17:22 +0100

    posts module

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Param,
+    Body,
+    NotFoundException,
+    UseGuards,
+    Request
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { PostsService } from "./posts.service";
+import { Post as PostEntity } from "./post.entity";
+import { PostDto } from "./dto/post.dto";
 
-@Controller('posts')
-export class PostsController {}
+@Controller("posts")
+export class PostsController {
+    constructor(private readonly PostsService: PostsService) {}
+    
+    
+}
