101af4bebe1c2576722faafd9fdd726aa823122e
anonfedora <musaeleazar090@gmail.com>
2024-04-11 23:45:31 +0100

    posts module: controller

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -17,7 +17,54 @@ import { PostDto } from "./dto/post.dto";
 
 @Controller("posts")
 export class PostsController {
-    constructor(private readonly PostsService: PostsService) {}
-    
-    
+    constructor(private readonly postService: PostsService) {}
+
+    @Get()
+    async findAll() {
+        return await this.postService.findAll();
+    }
+
+    @Get(":id")
+    async findOne(@Param("id") id: number): Promise<PostEntity> {
+        const post = await this.postService.findOne(id);
+
+        if (!post) {
+            throw new NotFoundException("This post does not exist");
+        }
+        return post;
+    }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Post()
+    async create(@Body() post: PostDto, @Request() req): Promise<PostEntity> {
+        return await this.postService.create(post, req.user.id);
+    }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Put(":id")
+    async update(
+        @Param("id") id: number,
+        @Body() post: PostDTO,
+        @Request() req
+    ): Promise<PostEntity> {
+        const { numberOfAffectedRows, updatedPost } =
+            await this.postService.update(id, post, req.user.id);
+
+        if (numberOfAffectedRows === 0) {
+            throw new NotFoundException("This Post doesn't exist");
+        }
+        return updatedPost;
+    }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Delete(":id")
+    async remove(@Param("id") id: number, @Request req) {
+        const deleted = await this.postService.delete(id, req.user.id);
+
+        if (deleted === 0) {
+            throw new NotFoundException("This post does not exist");
+        }
+
+        return "Deleted Successfully";
+    }
 }
