4a0e9d860ab85d3b07dc66b3bfc2fcb84b5847b6
anonfedora <musaeleazar090@gmail.com>
2024-06-19 09:39:51 +0100

    API Docs

--- src/modules/posts/dto/post.dto.ts
+++ src/modules/posts/dto/post.dto.ts
@@ -1,10 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, MinLength } from "class-validator";
 
 export class PostDto {
+    @ApiProperty({ type: String, example: "New Post" })
     @IsNotEmpty()
     @MinLength(4)
     private title: string;
 
+    @ApiProperty({
+        type: String,
+        example: "This is the content/body of the blog post."
+    })
     @IsNotEmpty()
     private body: string;
 }
