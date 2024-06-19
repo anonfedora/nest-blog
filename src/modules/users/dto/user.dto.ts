128981c8d55d44506981d35fa522f4e532611f8c
anonfedora <musaeleazar090@gmail.com>
2024-06-19 09:39:38 +0100

    API Docs

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,20 +1,33 @@
-import { IsNotEmpty, IsEnum, MinLength, IsEmail } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import {
+    IsNotEmpty,
+    IsString,
+    IsEnum,
+    MinLength,
+    IsEmail
+} from "class-validator";
+
 enum Gender {
     MALE = "male",
     FEMALE = "female"
 }
 export class UserDTO {
+    @ApiProperty({ type: String, example: "John Doe" })
     @IsNotEmpty()
+    @IsString()
     readonly name: string;
 
+    @ApiProperty({ type: String, example: "john@mail.com" })
     @IsNotEmpty()
     @IsEmail()
     readonly email: string;
 
+    @ApiProperty({ type: String, example: "password" })
     @IsNotEmpty()
     @MinLength(8)
     readonly password: string;
 
+    @ApiProperty({ example: "male" })
     @IsNotEmpty()
     @IsEnum(Gender, { message: "Gender must either be male or female" })
     readonly gender: Gender;
