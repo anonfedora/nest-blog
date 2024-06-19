2543f0b8066e4179189c9b960bd2b925ef993535
anonfedora <musaeleazar090@gmail.com>
2024-06-19 09:39:57 +0100

    API Docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidateInputPipe } from "./core/pipes/validate.pipe";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle("Mini Blog")
+        .setDescription("Mini Nest Blog API description")
+        .setVersion("0.1")
+        .build(); // Build the document
+
+    // Create a Swagger document using the application instance and the document configuration
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup("docs", app, document);
+
     app.setGlobalPrefix("api/v1");
     app.useGlobalPipes(new ValidateInputPipe());
     await app.listen(3000);
